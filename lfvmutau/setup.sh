2ac51a91a1d300faf5f8297f163fea8cbbbd733c
Aaron Levine <aglevine@wisc.edu>
2016-10-14 08:00:23 -0500

    Added TriMuon analyzer

--- lfvmutau/setup.sh
+++ lfvmutau/setup.sh
@@ -12,9 +12,9 @@ export jobid=LFV_sep16_v2
 export afile=`find $datasrc/$jobid | grep root | head -n 1`
 
 ## Build the cython wrappers
-rake "make_wrapper[$afile, eem/final/Ntuple, EEMuTree]"
+rake "make_wrapper[$afile, mmm/final/Ntuple, MuMuMuTree]"
 
 ls *pyx | sed "s|pyx|so|" | xargs rake 
 
-#rake "meta:getinputs[$jobid, $datasrc,eem/metaInfo, eem/summedWeights]"
+rake "meta:getinputs[$jobid, $datasrc,mmm/metaInfo, mmm/summedWeights]"
 #rake "meta:getmeta[inputs/$jobid, eem/metaInfo, 13, eem/summedWeights]"
